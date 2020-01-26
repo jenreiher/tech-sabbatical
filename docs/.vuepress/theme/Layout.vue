a0bdc17782aff48d7378bc858712500fa5e8a1eb
jenreiher <jenreiher@users.noreply.github.com>
2020-01-26 15:48:33 -0800

    Add home layout

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="theme-container">
-    <Content/>
+    <Home v-if="$page.frontmatter.home" />
+    <Content
+      v-else
+    />
   </div>
 </template>
 
+<script>
+import Home from '@theme/components/Home.vue'
+
 export default {
   name: 'Layout',
+  components: {
+    Home,
+  }
 }
 </script>
