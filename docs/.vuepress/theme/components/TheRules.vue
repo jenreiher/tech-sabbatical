f91d700f6ac15822a0f546dd03b2db4141f557e6
jenreiher <jenreiher@users.noreply.github.com>
2020-02-15 20:33:39 -0800

    Create Rules component

---
+++ docs/.vuepress/theme/components/TheRules.vue
@@ -0,0 +1,55 @@
+<template>
+<ol>
+  <li :class="successStyle(television)">
+    No television.
+  </li>
+  <li :class="successStyle(smartphones)">
+    No smartphone functionality.
+  </li>
+  <li :class="successStyle(voice)">
+    No voice interfaces without a direct analog equivalent. 
+  </li>
+  <li :class="successStyle(screens)">
+    No  devices with screen interfaces.
+  </li>
+</ol>
+
+</template>
+
+<script>
+  export default {
+    props: {
+      television: {
+        type: Boolean,
+        required: true,
+        default: true,
+      },
+      smartphones: {
+        type: Boolean,
+        required: true,
+        default: true,
+      },
+      screens: {
+        type: Boolean,
+        required: true,
+        default: true,
+      },
+      voice: {
+        type: Boolean,
+        required: true,
+        default: true,
+      },
+    },
+    methods: {
+      successStyle(prop) {
+        return this[prop] === false ? 'strike' : ''
+      }
+    }
+  }
+</script>
+
+<style>
+  .strike {
+    text-decoration: line-through;
+  }
+</style>
